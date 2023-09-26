06abbcf7ee83c72da7bbf367cd18ad40154c1829
Autumrose <32555511+Autumrose@users.noreply.github.com>
2023-09-26 11:43:52 -0600

    Add files via upload

---
+++ FormulaEvaluator/FormulaEvaluator.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace FormulaEvaluator
+{
+    public delegate int Lookup(String v);
+
+    /// <summary>
+    /// Evaluate arithmetic expressions using standard infix notation.
+    /// </summary>
+    public class Evaluator
+    {
+        /// <summary>
+        /// This function takes an expression as a String and returns the computed value in an int, or throw an ArgumentException if the expression is not computable.
+        /// </summary>
+        public static int Evaluate(String exp, Lookup variableEvaluator)
+        {
+            Stack<string> operatorStack = new Stack<string>();
+            Stack<int> valueStack = new Stack<int>();
+
+            exp = exp.Trim();
+            string[] substrings = Regex.Split(exp, "(\\()|(\\))|(-)|(\\+)|(\\*)|(/)");
+
+            foreach (String token in substrings)
+            {
+                if (String.IsNullOrEmpty(token))
+                {
+                    continue;
+                }
+
+                int intValue;
+                int computed;
+                if (int.TryParse(token, out intValue)) // if token is int
+                {
+                    if (operatorStack.Count > 0)
+                    {
+                        if (Equals(operatorStack.Peek(), "*") || Equals(operatorStack.Peek(), "/")) // if operator is * or /
+                        {
+                            if (valueStack.Count == 0) // value stack empty error
+                            {
+                                throw new ArgumentException("Value stack is empty!");
+                            }
+
+                            string op = operatorStack.Pop();
+                            int val = valueStack.Pop();
+
+                            if (intValue == 0 && Equals(op, "/")) // divide by 0 error
+                            {
+                                throw new ArgumentException();
+                            }
+                            else
+                            {
+                                if (Equals(op, "*"))
+                                {
+                                    computed = val * intValue;
+                                    valueStack.Push(computed); // apply *
+                                }
+                                else if (Equals(op, "/"))
+                                {
+                                    computed = val / intValue;
+                                    valueStack.Push(computed); // apply /
+                                }
+                            }
+                        }
+                        else
+                        {
+                            valueStack.Push(intValue); // else push to value stack
+                        }
+                    }
+                    else
+                    {
+                        valueStack.Push(intValue); // else push to value stack
+                    }
+                }
+                else if (Regex.IsMatch(token, @"[a-zA-Z]+\d+")) // if token is a variable
+                {
+                    int varVal = variableEvaluator(token);
+                    if (operatorStack.Count > 0)
+                    {
+                        if (Equals(operatorStack.Peek(), "*") || Equals(operatorStack.Peek(), "/")) // if operator is * or /
+                        {
+                            if (valueStack.Count == 0) // value stack empty error
+                            {
+                                throw new ArgumentException("Value stack is empty!");
+                            }
+
+                            string op = operatorStack.Pop();
+                            int val = valueStack.Pop();
+
+                            if (varVal == 0 && Equals(op, "/")) // divide by 0 error
+                            {
+                                throw new ArgumentException();
+                            }
+                            else
+                            {
+                                if (Equals(op, "*"))
+                                {
+                                    computed = val * varVal;
+                                    valueStack.Push(computed); // apply *
+                                }
+                                else if (Equals(op, "/"))
+                                {
+                                    computed = val / varVal;
+                                    valueStack.Push(computed); // apply /
+                                }
+                            }
+                        }
+                        else
+                        {
+                            valueStack.Push(varVal); // else push to value stack
+                        }
+                    }
+                    else
+                    {
+                        valueStack.Push(varVal); // else push to value stack
+                    }
+                }
+                else if (Equals(token, "+") || Equals(token, "-")) // if token is + or -
+                {
+                    if (operatorStack.Count > 0)
+                    {
+                        if (Equals(operatorStack.Peek(), "+") || Equals(operatorStack.Peek(), "-"))
+                        {
+                            if (valueStack.Count < 2) // value stack less than 2
+                            {
+                                throw new ArgumentException();
+                            }
+
+                            string op = operatorStack.Pop();
+                            int firstVal = valueStack.Pop();
+                            int secondVal = valueStack.Pop();
+
+                            if (Equals(op, "-"))
+                            {
+                                computed = secondVal - firstVal;
+                                valueStack.Push(computed); // apply /
+                            }
+                            else if (Equals(op, "+"))
+                            {
+                                computed = firstVal + secondVal;
+                                valueStack.Push(computed); // apply /
+                            }
+                        }
+                    }
+                    else if (valueStack.Count < 1)
+                    {
+                        throw new ArgumentException();
+                    }
+                    operatorStack.Push(token);
+                }
+                else if (Equals(token, "*") || Equals(token, "/")) // if token is * or /
+                {
+                    if (valueStack.Count < 1)
+                    {
+                        throw new ArgumentException();
+                    }
+                    operatorStack.Push(token);
+                }
+                else if (Equals(token, "(")) // if token is (
+                {
+                    operatorStack.Push(token);
+                }
+                else if (Equals(token, ")")) // if token is ) 
+                {
+                    if (operatorStack.Count > 0)
+                    {
+                        if (Equals(operatorStack.Peek(), "+") || Equals(operatorStack.Peek(), "-"))
+                        {
+                            if (valueStack.Count < 2) // value stack less than 2
+                            {
+                                throw new ArgumentException();
+                            }
+
+                            string op = operatorStack.Pop();
+                            int firstVal = valueStack.Pop();
+                            int secondVal = valueStack.Pop();
+
+                            if (Equals(op, "-"))
+                            {
+                                computed = secondVal - firstVal;
+                                valueStack.Push(computed); // apply /
+                            }
+                            else if (Equals(op, "+"))
+                            {
+                                computed = firstVal + secondVal;
+                                valueStack.Push(computed); // apply /
+                            }
+
+                            if (operatorStack.Count < 1)
+                            {
+                                throw new ArgumentException();
+                            }
+                            else if (!operatorStack.Pop().Equals("("))
+                            {
+                                throw new ArgumentException();
+                            }
+                            else if (operatorStack.Count > 0)
+                            {
+                                if (Equals(operatorStack.Peek(), "*") || Equals(operatorStack.Peek(), "/"))
+                                {
+                                    if (valueStack.Count < 2) // value stack less than 2
+                                    {
+                                        throw new ArgumentException();
+                                    }
+
+                                    string op2 = operatorStack.Pop();
+                                    int firstVal2 = valueStack.Pop();
+                                    int secondVal2 = valueStack.Pop();
+
+                                    if (Equals(op2, "*"))
+                                    {
+                                        computed = firstVal2 * secondVal2;
+                                        valueStack.Push(computed); // apply *
+                                    }
+                                    else if (Equals(op2, "/"))
+                                    {
+                                        computed = secondVal2 / firstVal2;
+                                        if (firstVal2 == 0)
+                                        {
+                                            throw new ArgumentException();
+                                        }
+                                        valueStack.Push(computed); // apply /
+                                    }
+
+                                }
+                            }
+                        }else if (!operatorStack.Pop().Equals("("))
+                        {
+                            throw new ArgumentException();
+                        }
+                    }
+                }
+                else // token did not match any acceptable conditions
+                {
+                    throw new ArgumentException();
+                }
+            }
+
+            // After all tokens are processed
+            if (operatorStack.Count == 0)
+            {
+                if (valueStack.Count != 1)
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    return valueStack.Pop();
+                }
+            }
+            else
+            {
+                if ((operatorStack.Count == 1 && valueStack.Count != 2) || (operatorStack.Count != 1 && valueStack.Count == 2))
+                {
+                    throw new ArgumentException();
+                }
+                else
+                {
+                    string op1 = operatorStack.Pop();
+                    int val1 = valueStack.Pop();
+                    int val2 = valueStack.Pop();
+
+                    if (Equals(op1, "-"))
+                    {
+                        return val2 - val1;
+                    }
+                    else if (Equals(op1, "+"))
+                    {
+                        return val2 + val1;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
+
---
+++ FormulaEvaluatorApp/FormulaEvalutorApp.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FormulaEvaluator;
+
+namespace FormulaEvaluatorApp
+{
+    /// <summary>
+    /// This class provides tests for the evaluator.
+    ///</summary>
+    class Program
+    {
+        public static int returnVarValue(string variable)
+        {
+            if (variable == "X1")
+            {
+                return 1;
+            } else
+            {
+                throw new ArgumentException();
+            }
+        }
+
+        static Lookup varEvaluator = new Lookup(returnVarValue);
+
+        static void Main(string[] args)
+        {
+            // TestPlus
+            if (Evaluator.Evaluate("2+2", varEvaluator) == 4)
+            {
+                Console.WriteLine("Testing plus: PASSED");
+            } else
+            {
+                Console.WriteLine("Testing plus: FAILED");
+            }
+
+            // TestPlusWithWhitespace
+            if (Evaluator.Evaluate("2 + 2", varEvaluator) == 4)
+            {
+                Console.WriteLine("Testing plus with whitespace: PASSED");
+            }
+            else
+            {
+                Console.WriteLine("Testing plus with whitespace: FAILED");
+            }
+
+            // TestMinus
+            if (Evaluator.Evaluate("4-2", varEvaluator) == 2)
+            {
+                Console.WriteLine("Testing plus: PASSED");
+            }
+            else
+            {
+                Console.WriteLine("Testing plus: FAILED");
+            }
+
+            // TestPlusWithVariable
+            if (Evaluator.Evaluate("2+X1", varEvaluator) == 3)
+            {
+                Console.WriteLine("Testing plus with variable: PASSED");
+            }
+            else
+            {
+                Console.WriteLine("Testing plus with variable: FAILED");
+            }
+
+            // TestInvalidVariable
+            try {
+                Evaluator.Evaluate("2+AB", varEvaluator);
+                Console.WriteLine("Testing invalid variable error: FAILED");
+            } catch (ArgumentException)
+            {
+                Console.WriteLine("Testing invalid variable error: PASSED");
+            }
+
+            // TestMultiply
+            if (Evaluator.Evaluate("2*2", varEvaluator) == 4)
+            {
+                Console.WriteLine("Testing multiply: PASSED");
+            }
+            else
+            {
+                Console.WriteLine("Testing multiply: FAILED");
+            }
+
+            // TestDivide
+            if (Evaluator.Evaluate("4/2", varEvaluator) == 2)
+            {
+                Console.WriteLine("Testing divide: PASSED");
+            }
+            else
+            {
+                Console.WriteLine("Testing divide: FAILED");
+            }
+
+            // TestDivideByZero
+            try
+            {
+                Evaluator.Evaluate("2/0", varEvaluator);
+                Console.WriteLine("Testing divide by 0 error: FAILED");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Testing divide by 0 error: PASSED");
+            }
+
+            // TestParentheses
+            if (Evaluator.Evaluate("(4/2)+2", varEvaluator) == 4)
+            {
+                Console.WriteLine("Testing parentheses: PASSED");
+            }
+            else
+            {
+                Console.WriteLine("Testing parentheses: FAILED");
+            }
+
+            // TestInvalidExpression
+            try
+            {
+                Evaluator.Evaluate("2++", varEvaluator);
+                Console.WriteLine("Testing invalid expression error: FAILED");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Testing invalid expression error: PASSED");
+            }
+
+            Console.ReadLine();
+        }
+       
+    }
+}
